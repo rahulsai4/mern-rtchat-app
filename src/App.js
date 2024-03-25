9e2f5aeca06385af2292ae6d19c6995821c84e82
rahul <114241667+rahulsai4@users.noreply.github.com>
2024-03-25 20:55:08 +0530

    made the app cover 100% of screen

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import Signin from "./pages/Signin";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export default function App() {
-    return (
-        <>
-            <Home />
-        </>
-    );
+    return <Home />;
 }
