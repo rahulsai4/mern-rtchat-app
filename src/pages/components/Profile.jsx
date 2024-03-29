722c3866589ed6aa7522feda7dfee6373eb35d0e
rahul <114241667+rahulsai4@users.noreply.github.com>
2024-03-29 17:54:16 +0530

    changed images

--- src/pages/components/Profile.jsx
+++ src/pages/components/Profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Profile = () => {
     return (
         <div className="profile">
-            <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D"></img>
+            <img src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?size=626&ext=jpg"></img>
             <span>my name</span>
             <button>logout</button>
         </div>
